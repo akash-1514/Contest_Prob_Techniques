b0e6dd09d2b004d1aa03d1606195ff057db7321a
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-07-20 01:38:10 -0700

    Time: 5 ms (28.40%), Space: 7.5 MB (94.44%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[101];
+    int helper(vector<int>&arr, int idx) {
+        if(idx >= arr.size()) {
+            return 0;
+        }
+        
+        if(dp[idx] != -1) return dp[idx];
+        
+        int pick = arr[idx] + helper(arr, idx + 2);
+        int notPick = helper(arr, idx + 1);
+        
+        return dp[idx] = max(pick, notPick);
+    }
+    int rob(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return helper(nums, 0);
+    }
+};
