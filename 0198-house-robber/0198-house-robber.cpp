0a9768d196a4247b898a74a5ebac8c6868a74c53
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-07-20 01:40:28 -0700

    Time: 0 ms (100.00%), Space: 7.6 MB (94.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int dp[101];
     int helper(vector<int>&arr, int idx) {
-        if(idx >= arr.size()) {
+        if(idx == arr.size()) {
             return 0;
         }
         
         if(dp[idx] != -1) return dp[idx];
         
-        int pick = arr[idx] + helper(arr, idx + 2);
+        int pick = arr[idx];
+        if(idx + 2 <= arr.size()) {
+            pick += helper(arr, idx + 2);
+        }
         int notPick = helper(arr, idx + 1);
         
         return dp[idx] = max(pick, notPick);
