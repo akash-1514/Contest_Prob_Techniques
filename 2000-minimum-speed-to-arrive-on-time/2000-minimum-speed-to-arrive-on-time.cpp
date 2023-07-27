b4ea0bd2a63d0ab677c415082d13846ce7484701
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-07-27 13:19:06 -0700

    Time: 291 ms (86.1%), Space: 101.5 MB (61.47%) - LeetHub

---
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool pred(vector<int>&dist, int mid, double hour) {
+        double time = 0.0;
+        for(int i = 0; i < dist.size(); ++i) {
+            if(i == dist.size() - 1) {
+                time += (1.0 * dist[i] / mid);
+            } else {
+                time += ceil((1.0 * dist[i]) / mid);
+            }
+        }
+        return time <= hour;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int low = 1, high = 1e9, ans = -1;
+        while(low <= high) {
+            int mid = (low + high) >> 1;
+            cout << mid << endl;
+            if(pred(dist, mid, hour)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
