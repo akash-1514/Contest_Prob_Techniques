1ae4e3b293cbb8df8c927ed2c412804e0df6b90c
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-08-25 11:57:52 -0700

    Time: 550 ms (20.35%), Space: 115.1 MB (5.01%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     int dp[100001];
-    int bs(vector<vector<int>>&inter, int x) {
-        int low = 0, high = inter.size() - 1;
-        int ans = inter.size();
-        while(low <= high) {
-            int mid = (low + high) >> 1;
-            if(inter[mid][0] >= x) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return ans;
-    }
-    int helper(vector<vector<int>>&intervals, int idx) {
-        if(idx >= intervals.size()) {
-            return 0;
-        }
+    int helper(vector<vector<int>>&arr, int idx) {
+        if(idx >= arr.size()) return 0;
         
         if(dp[idx] != -1) return dp[idx];
         
-        vector<int>v = {intervals[idx][1], INT_MIN};
-        auto i = lower_bound(intervals.begin(), intervals.end(), v) - intervals.begin();
+        vector<int>v = {arr[idx][1], INT_MIN};
+        auto nextIdx = lower_bound(arr.begin(), arr.end(), v) - arr.begin();
         
-        // auto i = bs(intervals, intervals[idx][1]);
-        
-        int mx = 0;
-        mx = max(mx, 1 + helper(intervals, i));
-        mx = max(mx, helper(intervals, idx + 1));
-        return dp[idx] = mx;
+        int pick = 1 + helper(arr, nextIdx);
+        int notPick = helper(arr, idx + 1);
+        return dp[idx] = max(pick, notPick);
     }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
         memset(dp, -1, sizeof(dp));
-        return intervals.size() - helper(intervals, 0);
+        sort(intervals.begin(), intervals.end());
+        return n - helper(intervals, 0);
     }
 };
