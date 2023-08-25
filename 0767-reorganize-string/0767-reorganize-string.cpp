450e40a965853d5dee78417f9717cd7b3e15a808
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-08-25 01:47:27 -0700

    Time: 0 ms (100.00%), Space: 6.4 MB (32.86%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    string reorganizeString(string S) {
-	vector<int> cnt(26);
-	int mostFreq = 0, i = 0;
-
-	for(char c : S)
-		if(++cnt[c - 'a'] > cnt[mostFreq])
-			mostFreq = (c - 'a');
-
-	if(2 * cnt[mostFreq] - 1 > S.size()) return "";
-
-	while(cnt[mostFreq]) {
-		S[i] = ('a' + mostFreq);
-		i += 2;
-		cnt[mostFreq]--;
-	}
-
-	for(int j = 0; j < 26; j++) {
-		while(cnt[j]) {
-			if(i >= S.size()) i = 1;
-			S[i] = ('a' + j);
-			cnt[j]--;
-			i += 2;
-		}
-	}
-
-	return S;
-}
+    string reorganizeString(string s) {
+        unordered_map<char, int>mp;
+        for(char ch : s) {
+            mp[ch]++;
+        }
+        
+        priority_queue<pair<int, char>>pq;
+        
+        for(auto &it : mp) {
+            pq.push({it.second, it.first});
+        }
+        
+        string ans = "";
+        while(!pq.empty()) {
+            pair<int, char>tp = pq.top(); pq.pop();
+            ans.push_back(tp.second);
+            if(!pq.empty()) {
+                pair<int, char>tp2 = pq.top(); pq.pop();
+                ans.push_back(tp2.second);
+                if(tp2.first - 1 > 0) {
+                    pq.push({tp2.first - 1, tp2.second});
+                }
+            }
+            
+            if(tp.first - 1 > 0) {
+                pq.push({tp.first - 1, tp.second});
+            }
+        }
+        
+        for(int i = 0; i < ans.size() - 1; ++i) {
+            if(ans[i] == ans[i + 1]) return "";
+        }
+        
+        return ans;
+    }
 };
