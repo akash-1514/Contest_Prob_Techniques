195ed2daa91769f51d9a8fdafb7546cc04703a91
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-08-26 10:05:45 -0700

    Time: 247 ms (64.97%), Space: 10.5 MB (69.32%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    int dp[2501][2505];
-    int helper(vector<int>&nums, int idx, int prev) {
-        if(idx >= nums.size()) return 0;
+    // int dp[2501][2505];
+//     int helper(vector<int>&nums, int idx, int prev) {
+//         if(idx >= nums.size()) return 0;
         
-        if(dp[idx][prev + 1] != -1) return dp[idx][prev + 1];
+//         if(dp[idx][prev + 1] != -1) return dp[idx][prev + 1];
         
-        int mx = 0;
-        if(prev == -1) {
-            mx = max(mx, 1 + helper(nums, idx + 1, idx));
-        } else if(nums[idx] > nums[prev]) {
-            mx = max(mx, 1 + helper(nums, idx + 1, idx));
-        }
-        mx = max(mx, helper(nums, idx + 1, prev));
-        return dp[idx][prev + 1] = mx;
-    }
+//         int mx = 0;
+//         if(prev == -1) {
+//             mx = max(mx, 1 + helper(nums, idx + 1, idx));
+//         } else if(nums[idx] > nums[prev]) {
+//             mx = max(mx, 1 + helper(nums, idx + 1, idx));
+//         }
+//         mx = max(mx, helper(nums, idx + 1, prev));
+//         return dp[idx][prev + 1] = mx;
+//     }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return helper(nums, 0, -1);
+        int n = nums.size();
+        vector<int>dp(n, 1);
+        int mx = 1;
+        for(int i = 1; i < n; ++i) {
+            for(int j = 0; j < i; ++j) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            mx = max(mx, dp[i]);
+        }
+        return mx;
     }
 };
