8355f80edf823056d91aa96eacf271a582cf4de0
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-01 04:14:53 -0700

    Time: 1488 ms (5.06%), Space: 310.2 MB (5.25%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
+    map<pair<int, int>, int>dp;
+    int helper(vector<pair<int, int>>&vec, int idx, int maxEnd) {
+        if(maxEnd == vec.size() - 1) return 0;
+        if(idx >= vec.size()) return 1e9;
+        if(vec[idx].first > maxEnd) return 1e9;
+        
+        if(dp.find({idx, maxEnd}) != dp.end()) return dp[{idx, maxEnd}];
+        
+        int take = 1e9;
+        if(vec[idx].first <= maxEnd) {
+            take = 1 + helper(vec, idx + 1, (int)max(maxEnd, vec[idx].second));
+        }
+        
+        int notTake = helper(vec, idx + 1, maxEnd);
+        
+        return dp[{idx, maxEnd}] = min(take, notTake);
+    }
     int minTaps(int n, vector<int>& ranges) {
-        vector<vector<int>>vec;
-        for(int i = 0; i <= n; ++i) {
-            int l = max(0, i - ranges[i]);
-            int r = min(n, i + ranges[i]);
+        vector<pair<int, int>>vec;
+        int idx = 0;
+        for(auto &rang : ranges) {
+            int l = max(0, idx - rang);
+            int r = min(n, idx + rang);
             vec.push_back({l, r});
+            idx++;
         }
-        
         sort(vec.begin(), vec.end());
-        
-        int taps = 0, maxEnd = -1e9, currPos = 0, idx = 0, ans = 0;
-        
-        while(idx <= n) {
-            if(vec[idx][0] <= currPos) {
-                maxEnd = max(maxEnd, vec[idx][1]);
-                idx++;
-            } else {
-                if(maxEnd == -1e9 || maxEnd <= currPos) return -1;
-                else {
-                    ans++, currPos = maxEnd;
-                    maxEnd = -1e9;
-                    if(currPos == n) return ans;
-                }
-            }
-        }
-        return maxEnd == n ? ans + 1 : -1;
+        int res = helper(vec, 0, 0);
+        return res == 1e9 ? -1 : res;
     }
 };
