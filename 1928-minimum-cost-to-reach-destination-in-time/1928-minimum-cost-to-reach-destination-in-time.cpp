9c1450b3d69c2f4da3bead858da14ddbab1ab5b9
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-05 12:02:27 -0700

    Time: 279 ms (62.31%), Space: 28.9 MB (72.16%) - LeetHub

---
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int helper(vector<pair<int, int>> graph[], vector<int>&passingFee, int src, int maxTime) {
+        if(src == passingFee.size() - 1) {
+            return passingFee[src];
+        }
+        
+        // vis[src] = 1;
+        
+        if(dp[src][maxTime] != -1) return dp[src][maxTime];
+        
+        int mn = 1e9;
+        
+        for(auto &adj : graph[src]) {
+            int node = adj.first, t = adj.second;
+            if(t <= maxTime) {
+                mn = min(mn, passingFee[src] + helper(graph, passingFee, node, maxTime - t));
+            }
+        }
+        
+        return dp[src][maxTime] = mn;
+        
+        // vis[src] = 0;
+    }
+    int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
+        int n = passingFees.size();
+        vector<pair<int, int>>graph[n];
+        for(auto &e : edges) {
+            int u = e[0], v = e[1], t = e[2];
+            graph[u].push_back({v, t});
+            graph[v].push_back({u, t});
+        }
+        memset(dp, -1, sizeof(dp));
+        int res = helper(graph, passingFees, 0, maxTime);
+        return res == 1e9 ? -1 : res;
+    }
+};
