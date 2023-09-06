329e5c6e7b9c1f9fc3a061ee3f9988902121f825
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-06 05:56:26 -0700

    Time: 120 ms (21.51%), Space: 46.8 MB (10.06%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,50 +1,97 @@
+map<int, set<string>>ans;
+class DSU {
+public:
+	vector<int> rank, size, parent;
+	DSU(int n) {
+        ans.clear();
+		rank.resize(n + 1, 0);
+		size.resize(n + 1);
+		parent.resize(n + 1, 0);
+		for (int i = 0; i <= n; ++i) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+
+	int findPar(int u) {
+		if (parent[u] == u) return u;
+		return parent[u] = findPar(parent[u]);
+	}
+
+	void unionByRank(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+		if (par_u == par_v) return;
+		int rank_u = rank[par_u];
+		int rank_v = rank[par_v];
+		if (rank_v == rank_u) {
+			rank[par_u]++;
+			parent[par_v] = par_u;
+		} else if (rank_u > rank_v) {
+			parent[par_v] = par_u;
+		} else {
+			parent[par_u] = par_v;
+		}
+	}
+
+	void unionBySize(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+
+		if(par_u == par_v) return;
+        
+		int size_u = size[par_u];
+		int size_v = size[par_v];
+
+		if(size_v > size_u) {
+            for(auto &it : ans[par_u]) {
+                ans[par_v].insert(it);
+            }
+			parent[par_u] = par_v;
+			size[par_v] += size[par_u];
+		} else {
+            for(auto &it : ans[par_v]) {
+                ans[par_u].insert(it);
+            }
+			parent[par_v] = par_u;
+			size[par_u] += size[par_v];
+		}
+   	}
+};
 class Solution {
 public:
-    map<int, set<string>>ans;
-    void dfs(vector<vector<string>>&accounts, vector<int> graph[], int src, int curr, vector<int>&vis) {
-        vis[src] = 1;
-        for(int i = 1; i < accounts[src].size(); ++i) {
-            ans[curr].insert(accounts[src][i]);
-        }
-        
-        for(auto &adj : graph[src]) {
-            if(!vis[adj]) {
-                dfs(accounts, graph, adj, curr, vis);
-            }
-        }
-    }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();
-        vector<int> graph[n];
+        DSU ds(n);
+        for(int i = 0; i < n; ++i) {
+            for(int j = 1; j < accounts[i].size(); ++j) {
+                ans[i].insert(accounts[i][j]);
+            }
+        }
         unordered_map<string, int>mp;
         for(int i = 0; i < n; ++i) {
             for(int j = 1; j < accounts[i].size(); ++j) {
-                if(mp.find(accounts[i][j]) != mp.end()) {
-                    graph[i].push_back(mp[accounts[i][j]]);
-                    graph[mp[accounts[i][j]]].push_back(i);
+                string email = accounts[i][j];
+                if(mp.find(email) != mp.end()) {
+                    ds.unionBySize(i, mp[email]);
                 } else {
-                    mp[accounts[i][j]] = i;
+                    mp[email] = i;
                 }
             }
         }
         
-        vector<int> vis(n, 0);
+        vector<vector<string>>a;
         for(int i = 0; i < n; ++i) {
-            if(!vis[i]) {
-                dfs(accounts, graph, i, i, vis);
+            if(ds.parent[i] == i) {
+                if(ans[i].size() != 0) {
+                    vector<string>res = {accounts[i][0]};
+                    for(auto &it : ans[i]) {
+                        res.push_back(it);
+                    }
+                    a.push_back(res);
+                }
             }
         }
-        
-        vector<vector<string>>res;
-        
-        for(auto &it : ans) {
-            vector<string>r = {accounts[it.first][0]};
-            for(auto &it2 : it.second) {
-                r.push_back(it2);
-            }
-            res.push_back(r);
-        }
-        
-        return res;
+        return a;
     }
 };
