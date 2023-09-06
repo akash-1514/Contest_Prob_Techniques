98709da547f7aaa714d75a61d44d9cd076c5ebce
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-06 05:17:04 -0700

    Time: 19 ms (88.08%), Space: 15.4 MB (88.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,23 +1,73 @@
+class DSU {
+public:
+	vector<int> rank, size, parent;
+public:
+    int components;
+	DSU(int n) {
+        components = n;
+		rank.resize(n + 1, 0);
+		size.resize(n + 1);
+		parent.resize(n + 1, 0);
+		for (int i = 0; i <= n; ++i) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+
+	int findPar(int u) {
+		if (parent[u] == u) return u;
+		return parent[u] = findPar(parent[u]);
+	}
+
+	void unionByRank(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+		if (par_u == par_v) return;
+		int rank_u = rank[par_u];
+		int rank_v = rank[par_v];
+		if (rank_v == rank_u) {
+			rank[par_u]++;
+			parent[par_v] = par_u;
+		} else if (rank_u > rank_v) {
+			parent[par_v] = par_u;
+		} else {
+			parent[par_u] = par_v;
+		}
+	}
+
+	void unionBySize(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+
+		if(par_u == par_v) return;
+
+        components--;
+		int size_u = size[par_u];
+		int size_v = size[par_v];
+
+		if(size_v > size_u) {
+			parent[par_u] = par_v;
+			size[par_v] += size[par_u];
+		} else {
+			parent[par_v] = par_u;
+			size[par_u] += size[par_v];
+		}
+   	}
+};
 class Solution {
 public:
-    void dfs(vector<vector<int>>&graph, int src, vector<int>&vis) {
-        vis[src] = 1;
-        for(int nodes = 0; nodes < graph[src].size(); ++nodes) {
-            if(nodes == src) continue;
-            if(!vis[nodes] && graph[src][nodes] == 1) {
-                dfs(graph, nodes, vis);
-            }
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size(), components = 0;
-        vector<int>vis(n, 0);
+        int n = isConnected.size();
+        DSU ds(n);
         for(int i = 0; i < n; ++i) {
-            if(!vis[i]) {
-                components++;
-                dfs(isConnected, i, vis);
+            for(int j = 0; j < n; ++j) {
+                if(i == j) continue;
+                if(isConnected[i][j]) {
+                    ds.unionBySize(i, j);
+                }
             }
         }
-        return components;
+        
+        return ds.components;
     }
 };
