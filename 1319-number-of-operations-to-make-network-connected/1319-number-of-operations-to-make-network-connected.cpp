10165ac8e605dbdbe8a226475b5462128f546b2f
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-06 05:28:11 -0700

    Time: 89 ms (86.97%), Space: 38.2 MB (84.15%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,76 @@
+class DSU {
+private:
+	vector<int> rank, size, parent;
+public:
+    int components, extraEdges = 0;
+	DSU(int n) {
+        components = n;
+        extraEdges = 0;
+		rank.resize(n + 1, 0);
+		size.resize(n + 1);
+		parent.resize(n + 1, 0);
+		for (int i = 0; i <= n; ++i) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+
+	int findPar(int u) {
+		if (parent[u] == u) return u;
+		return parent[u] = findPar(parent[u]);
+	}
+
+	void unionByRank(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+		if (par_u == par_v) {
+            return;
+        }
+		int rank_u = rank[par_u];
+		int rank_v = rank[par_v];
+		if (rank_v == rank_u) {
+			rank[par_u]++;
+			parent[par_v] = par_u;
+		} else if (rank_u > rank_v) {
+			parent[par_v] = par_u;
+		} else {
+			parent[par_u] = par_v;
+		}
+	}
+
+	void unionBySize(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+
+		if(par_u == par_v) {
+            extraEdges++;
+            return;
+        }
+
+        components--;
+		int size_u = size[par_u];
+		int size_v = size[par_v];
+
+		if(size_v > size_u) {
+			parent[par_u] = par_v;
+			size[par_v] += size[par_u];
+		} else {
+			parent[par_v] = par_u;
+			size[par_u] += size[par_v];
+		}
+   	}
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DSU ds(n);
+        for(auto &e : connections) {
+            ds.unionBySize(e[0], e[1]);
+        }
+        
+        if(ds.extraEdges >= ds.components - 1) {
+            return ds.components - 1;
+        }
+        return -1;
+    }
+};
