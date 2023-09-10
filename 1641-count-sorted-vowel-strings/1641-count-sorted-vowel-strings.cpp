feff38ccc33510a9c203c09687a813c38f3e53f4
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-10 09:56:29 -0700

    Time: 2 ms (57.67%), Space: 6 MB (73.31%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    int dp[6][51];
     int helper(string &s, int idx, int n) {
         if(n == 0) return 1;
         if(idx == s.size()) return 0;
+        
+        if(dp[idx][n] != -1) return dp[idx][n];
         
         int cnt = 0;
         
@@ -10,9 +13,10 @@ public:
         
         cnt += helper(s, idx + 1, n);
         
-        return cnt;
+        return dp[idx][n] = cnt;
     }
     int countVowelStrings(int n) {
+        memset(dp, -1, sizeof(dp));
         string s = "aeiou";
         return helper(s, 0, n);
     }
