4c542c41e1cb82c85143f727fb6fc1fe4c32b6f5
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-10 13:29:58 -0700

    Time: 2 ms (97.79%), Space: 5.9 MB (98.49%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
-    int dp[3001][1001];
-    int helper(int startPos, int endPos, int k) {
-        if(k == 0) {
-            if(startPos == endPos) return 1;
-            return 0;
+    long long expo(unsigned long long int x, int y, int p) {
+        long long res = 1;
+        x = x % p;
+
+        while(y > 0) {
+            if(y & 1) 
+                res = (res * x) % p;
+            y = y >> 1;
+            x = (x * x) % p;
         }
-        
-        if(dp[startPos + 1000][k] != -1) return dp[startPos + 1000][k];
-        
-        int cnt = 0;
-        cnt = (cnt + helper(startPos - 1, endPos, k - 1)) % mod;
-        cnt = (cnt + helper(startPos + 1, endPos, k - 1)) % mod;
-        return dp[startPos + 1000][k] = cnt;
+        return res;
+    }
+    long long inverse(long long n) {
+        long long ans = expo(n, mod - 2, mod);
+        return ans ;
     }
     int numberOfWays(int startPos, int endPos, int k) {
-        memset(dp, -1, sizeof(dp));
-        return helper(startPos, endPos, k);
+        int extra = k - abs(endPos - startPos);
+        if(extra % 2 != 0 || abs(endPos - startPos) > k) return 0;
+        int r = 1;
+        for(int i = 0; i <= k; ++i) {
+            if(startPos + i - (k - i) == endPos) {
+                r = i;
+                break;
+            }
+        }
+        
+        long long fact[k + 1];
+        fact[0] = 1;
+        for(int i = 1; i <= k; i++ ) {
+            fact[i] = (fact[i - 1] * i) % mod;
+        }
+        int ans = fact[k] * inverse(fact[r]) % mod;
+        ans = ans * inverse(fact[k - r]) % mod;
+        ans = ans % mod;
+        return ans;
     }
 };
