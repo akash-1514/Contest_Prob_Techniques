f73dddb36a00ae0306491fdc92eea890625317cb
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-11 09:21:13 -0700

    Time: 1393 ms (5.01%), Space: 24.3 MB (57.19%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int dp[2001][2001];
+    bool helper(string &s, string &p, int idx1, int idx2) {
+        if(idx1 == s.size() && idx2 == p.size()) return true;
+        if(idx1 == s.size()) {
+            for(int i = idx2; i < p.size(); ++i) {
+                if(p[i] != '*') return false;
+            }
+            return true;
+        }
+        if(idx2 == p.size()) {
+            return false;
+        }
+        
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+        if(p[idx2] == '*' || p[idx2] == '?') {
+            if(p[idx2] == '*') {
+                int i = idx1;
+                while(i <= s.size()) {
+                    if(helper(s, p, i, idx2 + 1)) return dp[idx1][idx2] = true;
+                    i++;
+                } 
+            } else {
+                if(helper(s, p, idx1 + 1, idx2 + 1)) return dp[idx1][idx2] = true;
+            }
+        } else {
+            if(p[idx2] == s[idx1]) {
+                if(helper(s, p, idx1 + 1, idx2 + 1)) return dp[idx1][idx2] = true;
+            } else {
+                return dp[idx1][idx2] = false;
+            }
+        }
+        return dp[idx1][idx2] = false;
+    }
+    bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
+        return helper(s, p, 0, 0);
+    }
+};
