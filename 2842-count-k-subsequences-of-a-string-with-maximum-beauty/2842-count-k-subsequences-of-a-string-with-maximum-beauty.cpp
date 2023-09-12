bc69475ed6b4113d50b8d872aff1fec7153c8379
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-12 13:17:36 -0700

    Time: 997 ms (6.88%), Space: 303.3 MB (5.01%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -1,49 +1,68 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7;
-    unordered_map<string, int>dp;
-    int helper(vector<int>&freq, int idx, int k, int mx) {
-        if(k == 0) {
-            if(mx == 0) return 1;
-            return 0;
+    int mod = 1000000007;
+    const int N = 200005;
+    long long binExpo(long long a, long long b, long long m) {
+        if(b == 0) return 1;
+        
+        long long res = binExpo(a, b / 2, m);
+        if(b % 2 == 0) {
+            return (res * res) % m;
+        } else {
+            return ((a * res) % m * res) % m;
         }
-        
-        if(idx >= 26 || mx < 0) return 0;
-        
-        string seen = to_string(idx) + "," + to_string(k) + "," + to_string(mx);
-        
-        if(dp.find(seen) != dp.end()) return dp[seen];
-        
-        long long cnt = 0;
-        if(freq[idx] != 0) {
-            cnt = (cnt + freq[idx] * 1ll * helper(freq, idx + 1, k - 1, mx - freq[idx])) % mod;
-        }
-        
-        cnt = (cnt + helper(freq, idx + 1, k, mx)) % mod;
-        return dp[seen] = cnt;
     }
+    
+    long long modInverse(long long a, long long b, long long m) {
+        return (a * binExpo(b, m - 2, m)) % mod;
+    }
+    
     int countKSubsequencesWithMaxBeauty(string s, int k) {
+        vector<long long> fact(N + 1, 0);
+        fact[0] = fact[1] = 1;
+        for(int i = 2; i <= N; ++i) {
+            fact[i] = (fact[i - 1] * 1ll * i) % mod;
+        }
+        
         vector<int> freq(26, 0);
         for(char ch : s) {
             freq[ch - 'a']++;
         }
         
-        vector<int>vec;
-        for(int i = 0; i < 26; ++i) {
-            if(freq[i] != 0) {
-                vec.push_back(freq[i]);
+        sort(freq.begin(), freq.end());
+        
+        priority_queue<pair<int, int>>pq;
+        
+        for(int i = 25; i >= 0;) {
+            if(freq[i] == 0) break;
+            int cnt = 0, x = freq[i];
+            while(i >= 0 && freq[i] == x) {
+                i--, cnt++;
+            }
+            pq.push({x, cnt});
+        }
+        int ans = 1;
+        while(!pq.empty() && k > 0) {
+            auto tp = pq.top();
+            pq.pop();
+            
+            int val = tp.first, cnt = tp.second;
+            
+            if(cnt <= k) {
+                ans = (ans * 1ll * binExpo(val, cnt, mod)) % mod;
+                k -= cnt;
+            } else {
+                long long num = fact[cnt];
+                long long dem = (fact[k] * 1ll * fact[cnt - k]) % mod;
+                
+                ans = ((ans * 1ll * modInverse(num, dem, mod)) % mod * binExpo(val, k, mod)) % mod;
+                k = 0;
+                break;
             }
         }
         
-        if(vec.size() < k) return 0;
-        
-        sort(vec.begin(), vec.end());
-        
-        int mx = 0, temp = k;
-        for(int i = vec.size() - 1; i >= 0 && temp-- > 0; --i) {
-            mx += vec[i];
-        }
+        if(k != 0) return 0;
 
-        return helper(freq, 0, k, mx);
+        return ans;
     }
 };
