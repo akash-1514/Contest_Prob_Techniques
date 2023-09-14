af55ff8bd7fffa861190ce19a75446fed157422b
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-14 06:54:07 -0700

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -14,15 +14,26 @@ public:
         if(i == j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         int minCost = 1e9;
-        for(int k = i + 1; k <= j; ++k) {
-            int cost = (arr[i] * arr[k] * arr[j + 1]) + helper(arr, n, i, k - 1) + helper(arr, n, k, j);
+        for(int k = i; k < j; ++k) {
+            int cost = (arr[i - 1] * arr[k] * arr[j]) + helper(arr, n, i, k) + helper(arr, n, k + 1, j);
             minCost = min(minCost, cost);
         }
         return dp[i][j] = minCost;
     }
     int matrixMultiplication(int N, int arr[]) {
-        memset(dp, -1, sizeof(dp));
-        return helper(arr, N, 0, N - 2);
+        // memset(dp, -1, sizeof(dp));
+        vector<vector<int>>dp(N + 1, vector<int>(N + 1, 0));
+        for(int i = N - 2; i >= 1; --i) {
+            for(int j = i + 1; j <= N - 1; ++j) {
+                int minCost = 1e9;
+                for(int k = i; k < j; ++k) {
+                    int cost = arr[i - 1] * arr[k] * arr[j] + dp[i][k] + dp[k + 1][j];
+                    minCost = min(minCost, cost);
+                }
+                dp[i][j] = minCost;
+            }
+        }
+        return dp[1][N - 1];
     }
 };
 
