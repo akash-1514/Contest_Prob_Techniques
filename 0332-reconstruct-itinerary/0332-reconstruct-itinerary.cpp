fb055156cd83d4635d28562e20aaa907052a2bf6
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-13 21:01:13 -0700

    Time: 47 ms (10.92%), Space: 18.4 MB (9.34%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool dfs(map<string, multiset<string>>&graph, string src, vector<string>&ans, unordered_map<string, int>&vis, int n) {\
+        ans.push_back(src);
+        if(n == 0) return true;
+        for(auto &adj : graph[src]) {
+            string path = src + "," + adj;
+            if(vis[path] > 0) {
+                if(--vis[path] == 0) vis.erase(path);
+                if(dfs(graph, adj, ans, vis, n - 1)) return true;
+                vis[path]++;
+            }
+        }
+        ans.pop_back();
+        return false;
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        int n = tickets.size();
+        unordered_map<string, int>vis;
+        map<string, multiset<string>>graph;
+        for(auto &ticket : tickets) {
+            graph[ticket[0]].insert(ticket[1]);
+            string path = ticket[0] + "," + ticket[1];
+            vis[path]++;
+        }
+        vector<string> ans;
+        dfs(graph, "JFK", ans, vis, n);
+        return ans;
+    }
+};
