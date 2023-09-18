e4299be51a51f9b6109e87b8c055100bfe3d6033
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-18 10:10:26 -0700

    Time: 1178 ms (5.01%), Space: 71.9 MB (5.11%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n = graph.size();
+        queue<pair<int, pair<int, int>>>q;
+        
+        unordered_map<string, int>vis;
+        
+        for(int i = 0; i < n; ++i) {
+            string temp = to_string(-1) + "," + to_string(i) + "," + to_string(1 << i);
+            vis[temp] = 1;
+            q.push({0, {i, (1 << i)}});
+        }
+       
+        while(!q.empty()) {
+            auto front = q.front();
+            q.pop();
+            
+            int dist = front.first, node = front.second.first, mask = front.second.second;
+            
+            
+            if(mask == ((1 << n) - 1)) {
+                return dist;
+            }
+            
+            for(auto &adj : graph[node]) {
+                string temp = to_string(node) + "," + to_string(adj) + "," + to_string(mask | (1 << adj));
+                if(vis[temp] == 0) {
+                    vis[temp] = 1;
+                    q.push({dist + 1, {adj, mask | (1 << adj)}});
+                }
+            }
+        }
+        
+        return 0;
+    }
+};
