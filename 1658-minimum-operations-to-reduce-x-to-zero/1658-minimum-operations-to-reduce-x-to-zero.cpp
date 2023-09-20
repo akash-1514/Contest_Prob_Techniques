3086c3a7a83ba1680640a4586bdacf5487b696e6
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-20 04:59:43 -0700

    Time: 328 ms (21.08%), Space: 152 MB (22.91%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int totSum = 0, n = nums.size(), sum = 0, idx = 0;
-        unordered_map<int, int>mp;
+        int n = nums.size(), totSum = 0;
+        
         for(int ele : nums) {
-            sum += ele;
-            mp[sum] = idx++;
             totSum += ele;
         }
         
         if(x > totSum) return -1;
         
-        int tar = totSum - x, mx = INT_MIN;
+        unordered_map<int, int>mp;
+        int target = totSum - x, sum = 0, mx = INT_MIN;
         mp[0] = -1;
-        sum = 0;
+        // extra condition we missed
+        if(target == 0) return n;
+        
         for(int i = 0; i < n; ++i) {
             sum += nums[i];
-            if(mp.find(sum - tar) != mp.end()) {
-                mx = max(mx, i - mp[sum - tar]);
+            if(mp.find(sum - target) != mp.end()) {
+                mx = max(mx, i - mp[sum - target]);
             }
+            mp[sum] = i;
         }
+        
         if(mx == INT_MIN) return -1;
-        return nums.size() - mx;
+        return n - mx;
     }
 };
