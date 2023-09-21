13e32677760765bd2c47a5b70504cf251bffc77a
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-20 22:47:20 -0700

    Time: 8 ms (85.85%), Space: 10.5 MB (11.24%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int pred(vector<int>&nums, int k, int mid) {
+    int pred(vector<int>&nums, int mid) {
         int cnt = 0, i = 0;
         for(int j = 0; j < nums.size(); ++j) {
             while(nums[j] - nums[i] > mid) {
@@ -8,23 +8,20 @@ public:
             }
             cnt += (j - i);
         }
-        return cnt >= k;
+        return cnt;
     }
     int smallestDistancePair(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int low = 1e9, high = nums[nums.size() - 1] - nums[0];
-        for(int i = 0; i < nums.size() - 1; ++i) {
-            low = min(low, nums[i + 1] - nums[i]);
-        }
-        
-        while(low < high) {
+        int low = 0, high = nums[nums.size() - 1] - nums[0], ans = 0;
+        while(low <= high) {
             int mid = (low + high) >> 1;
-            if(pred(nums, k, mid)) {
-                high = mid;
+            if(pred(nums, mid) >= k) {
+                ans = mid;
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return high;
+        return ans;
     }
 };
