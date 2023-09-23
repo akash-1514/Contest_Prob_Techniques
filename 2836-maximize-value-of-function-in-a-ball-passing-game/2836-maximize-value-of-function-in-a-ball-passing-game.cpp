3293ab2986014849131256601459108688c84112
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-22 21:04:06 -0700

    Time: 833 ms (32.92%), Space: 336.6 MB (20.76%) - LeetHub

--- 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.cpp
+++ 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    int maxUp = 35;
-    long long ans_query(vector<vector<pair<long long, long long>>>&dp, int node, long long k) {
-        if(k == 0) return 0;
-        
-        for(int i = 0; i < maxUp; ++i) {
-            if((1ll << i) & k) {
+    #define maxUp 33
+    long long ans_query(vector<vector<pair<int, long long>>>&dp, int node, long long k) {
+        for(int i = maxUp; i >= 0; --i) {
+            if((k & (1ll << i)) != 0) {
                 return dp[node][i].second + ans_query(dp, dp[node][i].first, k - (1ll << i));
             }
         }
@@ -13,23 +11,24 @@ public:
     }
     long long getMaxFunctionValue(vector<int>& receiver, long long k) {
         int n = receiver.size();
-        vector<vector<pair<long long, long long>>>dp(n, vector<pair<long long, long long>>(maxUp + 1));
+        
+        vector<vector<pair<int, long long>>>dp(n, vector<pair<int, long long>>(maxUp + 1));
+        
         for(int i = 0; i < n; ++i) {
             dp[i][0] = {receiver[i], receiver[i]};
         }
         
-        for(int i = 1; i <= maxUp; i++) {
-            for(int j = 0; j < n; ++j) {
-                int x = dp[j][i - 1].first;
-                dp[j][i].first = dp[x][i - 1].first;
-                dp[j][i].second = dp[j][i - 1].second + dp[x][i - 1].second;
+        // kth parent of each node and score from a node to its kth parent
+        for(int j = 1; j <= maxUp; ++j) {
+            for(int i = 0; i < n; ++i) {
+                dp[i][j].first = dp[dp[i][j - 1].first][j - 1].first;
+                dp[i][j].second = dp[i][j - 1].second + dp[dp[i][j - 1].first][j - 1].second;
             }
         }
-
+        
         long long mx = 0;
         for(int i = 0; i < n; ++i) {
-            long long score = i + ans_query(dp, i, k);
-            mx = max(mx, score);
+            mx = max(mx, i + ans_query(dp, i, k));
         }
         return mx;
     }
