d9dcf9888f71fae5e029854d4146ab375f1cabf2
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-25 20:24:00 -0700

    Time: 0 ms (100.00%), Space: 7.2 MB (10.66%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> vec1(26, 0), vis(26, 0);
-        for(int i = 0; i < s.size(); ++i) {
-            vec1[s[i] - 'a'] = i;
-        }
         stack<char>st;
+        vector<int> lastIndex(26, 0), isOccur(26, 0);
         for(int i = 0; i < s.size(); ++i) {
-            if(vis[s[i] - 'a']) continue;
-            while(!st.empty() && vec1[st.top() - 'a'] >= i && st.top() > s[i]) {
-                vis[st.top() - 'a'] = 0;
+            lastIndex[s[i] - 'a'] = i;
+        }
+        
+        for(int i = 0; i < s.size(); ++i) {
+            if(isOccur[s[i] - 'a']) continue;
+            while(!st.empty() && st.top() >= s[i] && lastIndex[st.top() - 'a'] >= i) {
+                isOccur[st.top() - 'a'] = 0;
                 st.pop();
             }
-            vis[s[i] - 'a'] = 1;
+            isOccur[s[i] - 'a'] = 1;
             st.push(s[i]);
-        } 
-        
+        }
         string ans = "";
         while(!st.empty()) {
             ans += st.top();
