09be8eba908a1f25be013fc56fb04b78f79d8a88
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-29 21:41:30 -0700

    Time: 55 ms (32.30%), Space: 54.7 MB (12.53%) - LeetHub

---
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        int mx = nums[0] > right ? 0 : -1;
+        dp[0] = (nums[0] >= left && nums[0] <= right);
+        for(int i = 1; i < n; ++i) {
+            if(nums[i] > right) {
+                dp[i] = 0;
+                mx = i;
+            } else {
+                if(nums[i] < left) dp[i] = dp[i - 1];
+                else dp[i] = i - mx;
+            }
+        }
+        
+        int sum = 0;
+        for(int ele : dp) {
+            sum += ele;
+        }
+        return sum;
+    }
+};
