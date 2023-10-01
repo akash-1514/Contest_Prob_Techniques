237eb56d373280fdbcf34249a6fd619d8867034b
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-09-30 23:19:12 -0700

    Time: 62 ms (100.00%), Space: 48.7 MB (57.14%) - LeetHub

--- 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.cpp
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.cpp
@@ -8,20 +8,17 @@ public:
         
         int x = (target / sum), rem = target % sum;
         
-        int sz = 1e9, i = 0, s = 0, ans = 1e9, sz2 = 1e9, s2 = 0, i2 = 0;
+        int sz = 1e9, i = 0, s = 0, ans = 1e9;
         for(int j = 0; j < arr.size(); ++j) {
             s += arr[j];
-            s2 += arr[j];
             while(s > rem) {
-                s2 -= arr[i2++];
                 s -= arr[i++];
             }
             if(s == rem) sz = min(sz, j - i + 1);
-            if(s2 == target) sz2 = min(sz2, j - i2 + 1);
         }
     
         if(sz != 1e9) {
-            ans = min({ans, sz2, x * (int)nums.size() + sz});
+            return x * nums.size() + sz;
         }
         
         return ans == 1e9 ? -1 : ans;
