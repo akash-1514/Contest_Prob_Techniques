0e6660c3122d40383a9fb9c737308a26d0fd3b33
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-10-10 11:04:20 -0700

    Time: 4 ms (58.90%), Space: 7.6 MB (23.31%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,41 +1,45 @@
 class Solution {
+private:
+    int obs = 0;
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
 public:
-    int cells = 0;
-    int helper(vector<vector<int>>&grid, vector<vector<int>>&vis, int x, int y, int cnt) {
-        if(grid[x][y] == 2) {
-            if(cnt == cells - 1) {
+    int helper(vector<vector<int>>&arr, int x, int y, int ex, int ey, int cnt, vector<vector<int>>&vis) {
+        int n = arr.size(), m = arr[0].size();
+        if(x == ex && y == ey) {
+            if(cnt == (n * m - obs - 1)) {
                 return 1;
-            } 
+            }
             return 0;
         }
-        int ways = 0;
-        vis[x][y] = 1;
-        int dx[] = {0, 1, 0, -1};
-        int dy[] = {1, 0, -1, 0};
+        
+        int paths = 0;
+        
+        vis[x][y] = 1;        
         for(int i = 0; i < 4; ++i) {
             int nx = x + dx[i], ny = y + dy[i];
-            if(nx >= 0 && ny >= 0 && nx < grid.size() && ny < grid[0].size() && !vis[nx][ny] && grid[nx][ny] != -1) {
-                ways += helper(grid, vis, nx, ny, cnt + 1);
+            if(nx >= 0 && ny >= 0 && nx < n && ny < m && arr[nx][ny] != -1 && !vis[nx][ny]) { 
+                paths += helper(arr, nx, ny, ex, ey, cnt + 1, vis);
             }
         }
         vis[x][y] = 0;
-        return ways;
+        return paths;
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        int sx = -1, sy = -1;
-        for(int  i = 0; i < n; ++i) {
+        int n = grid.size(), m = grid[0].size(), sx, sy, ex, ey;
+        for(int i = 0; i < n; ++i) {
             for(int j = 0; j < m; ++j) {
-                if(grid[i][j] != -1) cells++;
-                if(grid[i][j] == 1) {
+                if(grid[i][j] == 2) {
+                    ex = i, ey = j;
+                } else if(grid[i][j] == 1) {
                     sx = i, sy = j;
+                } else if(grid[i][j] == -1) {
+                    obs++;
                 }
             }
         }
         
-        
         vector<vector<int>>vis(n, vector<int>(m, 0));
-        
-        return helper(grid, vis, sx, sy, 0);
+        return helper(grid, sx, sy, ex, ey, 0, vis);
     }
 };
