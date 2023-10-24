14ec6935c65211787e5e9b7a299704b24973cae6
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-10-24 05:26:31 -0700

    Time: 196 ms (31.46%), Space: 90.3 MB (72.01%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int dp[501][501];
-    int helper(vector<int>&cost, vector<int>&time, int idx, int n) {
-        if(n <= 0) return 0;
-        if(idx >= cost.size()) return 1e9;
-
-        if(dp[idx][n] != -1) return dp[idx][n];
-
-        int mn = 1e9;
-        mn = min(mn, cost[idx] + helper(cost, time, idx + 1, n - time[idx] - 1));
-        mn = min(mn, helper(cost, time, idx + 1, n));
-        return dp[idx][n] = mn;
+    int helper(vector<int>&time, vector<int> &cost, int idx, int extraTime) {
+        int n = cost.size();
+        if(extraTime == n) {
+            return 0;
+        }
+        
+        if(idx >= n) {
+            return 1e9;    
+        }
+        
+        if(dp[idx][extraTime] != -1) return dp[idx][extraTime];
+        
+        int takePaidPainter = cost[idx] + helper(time, cost, idx + 1, min(n, extraTime + time[idx] + 1));
+        
+        int notTakePaidPainter = helper(time, cost, idx + 1, extraTime);
+        
+        return dp[idx][extraTime] = min(takePaidPainter, notTakePaidPainter);
     }
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
         memset(dp, -1, sizeof(dp));
-        return helper(cost, time, 0, n);
+        return helper(time, cost, 0, 0);
     }
 };
