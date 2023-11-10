72fcacc98584d4abb67da2deae3ba3753983cbf5
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-11-10 09:41:30 -0800

    Time: 82 ms (72.52%), Space: 63.3 MB (90.60%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        int mx = *max_element(arr.begin(), arr.end()), n = arr.size();
-        if(k >= arr.size() - 1) {
-            return mx;
-        }
-        deque<int>q;
-        for(int i = 0; i < n; ++i) {
-            q.push_back(arr[i]);
-        }
-        int i = 1, prev = -1, cnt = 0;
-        while(i <= n) {
-            int ele1 = q.front(); q.pop_front();
-            int ele2 = q.front(); q.pop_front();
-            if(ele1 > ele2) {
-                if(ele1 == prev) {
-                    cnt++; 
-                    if(cnt >= k) {
-                        return ele1;
-                    }
-                } else {
-                    cnt = 1;
-                    if(cnt >= k) return ele1;
-                    prev = ele1;
-                }
-                q.push_front(ele1);
-                q.push_back(ele2);
-            } else {
-                if(ele2 == prev) {
-                    cnt++;
-                    if(cnt >= k) return ele2;
-                } else {
-                    cnt = 1;
-                    if(cnt >= k) return ele2;
-                    prev = ele2;
-                }
-                q.push_back(ele1);
-                q.push_front(ele2);
-            }
+        int res = *max_element(arr.begin(), arr.end()), n = arr.size();
+        if(k >= n - 1) {
+            return res;
         }
         
-        return mx;
+        int mx = 0, cnt = 0, prev = -1;
+        for(int i = 0; i < n; ++i) {
+            mx = max(mx, arr[i]);
+            if(prev == mx) {
+                cnt++;
+                if(cnt >= k) return mx;
+            } else {
+                if(prev == -1) {
+                    cnt = 0;
+                } else {
+                    cnt = 1;
+                    if(cnt >= k) return mx;
+                }
+                prev = mx;
+            }
+        }
+        return res;
     }
 };
