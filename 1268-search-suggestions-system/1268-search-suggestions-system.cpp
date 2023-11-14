fa5d30ef4552cf45c1e19a19e7d24ee5899b65b2
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-11-14 02:47:29 -0800

    Time: 38 ms (74.19%), Space: 26.8 MB (83.96%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,99 +1,22 @@
-struct Node {
-    Node* links[26];
-    vector<string>suggestedWords;
-    bool isEnd = false;
-    
-    bool containsKey(char ch) {
-        return links[ch - 'a'];
-    }
-    
-    Node* get(char ch) {
-        return links[ch - 'a'];
-    }
-    
-    void put(char ch, Node* node) {
-        links[ch - 'a'] = node;
-    }
-};
-class Trie {
-public:
-	Node* root;
-	Trie() {
-		root = new Node();
-	}
-
-	// TC: O(length of word)
-	void insert(string word) {
-		Node* node = root;
-		for (char ch : word) {
-			if (!node->containsKey(ch)) {
-				node->put(ch, new Node());
-			}
-			node = node->get(ch);
-            node->suggestedWords.push_back(word);
-		}
-		node->isEnd = true;
-	}
-
-	// O(length of word)
-	bool search(string word) {
-		Node* node = root;
-		for (char ch : word) {
-			if(!node->containsKey(ch)) {
-				return false;
-			}
-			node = node->get(ch);
-		}
-		return node->isEnd;
-	}
-
-	// O(length of word)
-	bool startsWith(string prefix) {
-		Node* node = root;
-		for (char ch : prefix) {
-			if (!node->containsKey(ch)) {
-				return false;
-			}
-			node = node->get(ch);
-		}
-		return true;
-	}
-    
-    vector<vector<string>> suggestedProducts(string s) {
-        int n = s.size();
-        vector<vector<string>>ans(n);
-        Node* node = root;
-        int idx = 0;
-        for(char ch : s) {
-            if(!node->containsKey(ch)) {
-                break;
-            } else {
-                node = node->get(ch);
-                int cnt = 1;
-                vector<string>res;
-                for(auto &w : node->suggestedWords) {
-                    res.push_back(w);
-                    if(++cnt > 3) {
-                        break;
-                    }
-                }
-                ans[idx++] = res;
-            }
-        }
-        return ans;
-    }
-};
-
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        int n = products.size();
         sort(products.begin(), products.end());
-        Trie t;
-        for(auto &p : products) {
-            t.insert(p);
+        string prefix = "";
+        vector<vector<string>>ans;
+        int bStart = 0;
+        for(char ch : searchWord) {
+            prefix += ch;
+            
+            auto idx = lower_bound(products.begin() + bStart, products.end(), prefix) - products.begin();
+            
+            vector<string>res;
+            for(int i = idx; i < min((int)idx + 3, n) && !products[i].compare(0, prefix.size(), prefix); ++i) {
+                res.push_back(products[i]);
+            }
+            ans.push_back(res);
         }
-        
-        vector<vector<string>>ans = t.suggestedProducts(searchWord);
         return ans;
     }
 };
