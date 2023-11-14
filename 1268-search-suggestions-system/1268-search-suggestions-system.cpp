8625b8e161b783b7f052cb774a35a3c1bec26fcf
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-11-14 02:23:09 -0800

    Time: 158 ms (34.13%), Space: 133 MB (26.66%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,98 @@
+struct Node {
+    Node* links[26];
+    set<string>suggestedWords;
+    bool isEnd = false;
+    
+    bool containsKey(char ch) {
+        return links[ch - 'a'];
+    }
+    
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+    
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+};
+class Trie {
+public:
+	Node* root;
+	Trie() {
+		root = new Node();
+	}
+
+	// TC: O(length of word)
+	void insert(string word) {
+		Node* node = root;
+		for (char ch : word) {
+			if (!node->containsKey(ch)) {
+				node->put(ch, new Node());
+			}
+			node = node->get(ch);
+            node->suggestedWords.insert(word);
+		}
+		node->isEnd = true;
+	}
+
+	// O(length of word)
+	bool search(string word) {
+		Node* node = root;
+		for (char ch : word) {
+			if(!node->containsKey(ch)) {
+				return false;
+			}
+			node = node->get(ch);
+		}
+		return node->isEnd;
+	}
+
+	// O(length of word)
+	bool startsWith(string prefix) {
+		Node* node = root;
+		for (char ch : prefix) {
+			if (!node->containsKey(ch)) {
+				return false;
+			}
+			node = node->get(ch);
+		}
+		return true;
+	}
+    
+    vector<vector<string>> suggestedProducts(string s) {
+        int n = s.size();
+        vector<vector<string>>ans(n);
+        Node* node = root;
+        int idx = 0;
+        for(char ch : s) {
+            if(!node->containsKey(ch)) {
+                break;
+            } else {
+                node = node->get(ch);
+                int cnt = 1;
+                vector<string>res;
+                for(auto &w : node->suggestedWords) {
+                    res.push_back(w);
+                    if(++cnt > 3) {
+                        break;
+                    }
+                }
+                ans[idx++] = res;
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        Trie t;
+        for(auto &p : products) {
+            t.insert(p);
+        }
+        
+        vector<vector<string>>ans = t.suggestedProducts(searchWord);
+        return ans;
+    }
+};
