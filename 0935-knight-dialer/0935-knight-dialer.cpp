96f021524fba99d5ab58510c0eafcdff11ad6353
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-11-27 06:20:40 -0800

    Time: 99 ms (69.88%), Space: 29.1 MB (55.85%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -6,22 +6,26 @@ private:
 public:
     int knightDialer(int n) {
         // memset(dp, -1, sizeof(dp));
-        vector<vector<int>> dp(n + 1, vector<int>(10, 0));
-        for(int i = 0; i < 10; ++i) dp[1][i] = 1;
+        // vector<vector<int>> dp(n + 1, vector<int>(10, 0));
+        
+        // space optimization
+        vector<int> prev(10, 1);
         // dp[i][j] : total ways to to make distinct phone number of len = i starting from j
         for(int i = 2; i <= n; ++i) {
+            vector<int>curr(10, 0);
             for(int j = 0; j < 10; ++j) {
                 int cnt = 0;
                 for(int nPos : nextPos[j]) {
-                    cnt = (cnt + dp[i - 1][nPos]) % mod;
+                    cnt = (cnt + prev[nPos]) % mod;
                 }
-                dp[i][j] = cnt;
+                curr[j] = cnt;
             }
+            prev = curr;
         }
         
         int ans = 0;
         for(int i = 0; i < 10; ++i) {
-            ans = (ans + dp[n][i]) % mod;
+            ans = (ans + prev[i]) % mod;
         }
         return ans;
     }
