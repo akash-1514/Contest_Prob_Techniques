21450fa7653a70c3fc98a495e43c26864b40a7b2
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-11-27 06:10:20 -0800

    Time: 61 ms (82.89%), Space: 8.4 MB (72.08%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,52 +1,30 @@
 class Solution {
 private:
-    vector<vector<char>>vec;
     int mod = 1e9 + 7;
-    int dx[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
-    int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+    vector<vector<int>> nextPos = {
+        {4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
 public:
-    int dp[5001][4][3];
-    int helper(int n, int x, int y) {
+    int dp[5001][10];
+    int helper(int n, int nxt) {
         if(n == 1) {
             return 1;
         }
         
-        if(dp[n][x][y] != -1) return dp[n][x][y];
+        if(dp[n][nxt] != -1) return dp[n][nxt];
         
         int cnt = 0;
-        for(int i = 0; i < 8; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx >= 0 && ny >= 0 && nx < 4 && ny < 3 && (vec[nx][ny] >= '0' && vec[nx][ny] <= '9')) {
-                cnt = (cnt + helper(n - 1, nx, ny)) % mod;
-            }
+        
+        for(int nPos : nextPos[nxt]) {
+            cnt = (cnt + helper(n - 1, nPos)) % mod;
         }
-        return dp[n][x][y] = cnt;
+        
+        return dp[n][nxt] = cnt;
     }
     int knightDialer(int n) {
-        int cnt = 1;
-        vec.resize(4, vector<char>(3));
-        for(int i = 0; i < 4; ++i) {
-            for(int j = 0; j < 3; ++j) {
-                if(i == 3 && j == 0) vec[i][j] = '*';
-                else if(i == 3 && j == 1) vec[i][j] = '0';
-                else if(i == 3 && j == 2) vec[i][j] = '#';
-                else {
-                    vec[i][j] = (cnt + '0');
-                    cnt++;
-                }
-            }
-        }
-        
         memset(dp, -1, sizeof(dp));
-        
         int ans = 0;
-        for(int i = 0; i < 4; ++i) {
-            for(int j = 0; j < 3; ++j) {
-                if(vec[i][j] >= '0' && vec[i][j] <= '9') {
-                    ans = (ans + helper(n, i, j)) % mod;
-                }
-            }
+        for(int i = 0; i < 10; ++i) {
+            ans = (ans + helper(n, i)) % mod;
         }
         return ans;
     }
