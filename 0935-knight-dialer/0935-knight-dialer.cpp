b8028a64a20e2d8a0f6af493fdba6614f3fa4453
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-11-27 06:18:31 -0800

    Time: 116 ms (64.47%), Space: 37.7 MB (43.13%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -4,27 +4,24 @@ private:
     vector<vector<int>> nextPos = {
         {4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
 public:
-    int dp[5001][10];
-    int helper(int n, int nxt) {
-        if(n == 1) {
-            return 1;
-        }
-        
-        if(dp[n][nxt] != -1) return dp[n][nxt];
-        
-        int cnt = 0;
-        
-        for(int nPos : nextPos[nxt]) {
-            cnt = (cnt + helper(n - 1, nPos)) % mod;
-        }
-        
-        return dp[n][nxt] = cnt;
-    }
     int knightDialer(int n) {
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
+        vector<vector<int>> dp(n + 1, vector<int>(10, 0));
+        for(int i = 0; i < 10; ++i) dp[1][i] = 1;
+        // dp[i][j] : total ways to to make distinct phone number of len = i starting from j
+        for(int i = 2; i <= n; ++i) {
+            for(int j = 0; j < 10; ++j) {
+                int cnt = 0;
+                for(int nPos : nextPos[j]) {
+                    cnt = (cnt + dp[i - 1][nPos]) % mod;
+                }
+                dp[i][j] = cnt;
+            }
+        }
+        
         int ans = 0;
         for(int i = 0; i < 10; ++i) {
-            ans = (ans + helper(n, i)) % mod;
+            ans = (ans + dp[n][i]) % mod;
         }
         return ans;
     }
