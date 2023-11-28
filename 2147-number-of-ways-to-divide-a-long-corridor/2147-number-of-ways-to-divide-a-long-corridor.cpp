1fe8b65656785f8beb8db73925c7406c79adf9e9
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-11-28 03:09:58 -0800

    Time: 185 ms (41.14%), Space: 41.6 MB (54.86%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
-    int numberOfWays(string corridor) {
-        int ways = 1, n = corridor.size(), seats = 0, partition = 0;
-        bool found = false;
-        for(int i = 0; i < n; ++i) {
-            if(corridor[i] == 'S') {
-                if(seats == 2) {
-                    ways = (ways * 1ll * (partition + 1)) % mod;
-                    seats = 0, partition = 0;
-                }
-                seats++;
-                if(seats == 2) found = true;
-            } else {
-                if(seats == 2) {
-                    if(corridor[i] == 'P') {
-                        partition++;
-                    }
-                }
-            }
-        }
-        if((seats == 2 || seats == 0) && found) {
-            return ways;
-        } else {
+    int dp[100001][3];
+    long long mod = 1e9 + 7;
+    int helper(string &s, int idx, int seats) {
+        if(idx >= s.size()) {
+            if(seats == 2) return 1;
             return 0;
         }
+        
+        if(dp[idx][seats] != -1) return dp[idx][seats];
+        
+        int ways = 0;
+        if(s[idx] == 'S') {
+            ways = (ways + helper(s, idx + 1, (seats + 1) > 2 ? 1 : seats + 1)) % mod;
+        } else {
+            if(seats == 2) {
+                ways = (ways + helper(s, idx + 1, 0)) % mod;
+            }
+            ways = (ways + helper(s, idx + 1, seats)) % mod;
+        }
+        return dp[idx][seats] = ways;
+    }
+    int numberOfWays(string corridor) {
+        int n = corridor.size();
+        memset(dp, -1, sizeof(dp));
+        return helper(corridor, 0, 0);
     }
 };
