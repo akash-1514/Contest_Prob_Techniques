7d0612cce083657b92da6b0b2f7376117b64f19a
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-12-20 09:43:55 -0800

    Time: 395 ms (33.64%), Space: 152.1 MB (21.50%) - LeetHub

--- 2732-find-a-good-subset-of-the-matrix/2732-find-a-good-subset-of-the-matrix.cpp
+++ 2732-find-a-good-subset-of-the-matrix/2732-find-a-good-subset-of-the-matrix.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    bool valid(int curr, int prev) {
-        for(int j = 0; j < 32; ++j) {
-            if((curr & (1 << j)) != 0 && (prev & (1 << j)) != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
     vector<int> goodSubsetofBinaryMatrix(vector<vector<int>>& grid) {
         vector<int> ans;
         unordered_map<int, int>mp;
@@ -24,7 +16,7 @@ public:
         for(int i = 0; i < vec.size(); ++i) {
             if(vec[i] == 0) return {i};
             for(int j = 0; j < (1 << grid[0].size()); ++j) {
-                if(valid(vec[i], j)) {
+                if((vec[i] & j) == 0) {
                     if(mp.find(j) != mp.end()) {
                         return {mp[j], i};
                     }
