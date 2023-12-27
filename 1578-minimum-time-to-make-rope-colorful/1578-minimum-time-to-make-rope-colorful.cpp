d97b3ee59c2e22e558e019c8ddb9aa5b447eea74
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-12-26 21:29:59 -0800

    Time: 116 ms (67.51%), Space: 95.8 MB (49.48%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int dp[100001][27];
-    int helper(string &color, vector<int>&neededTime, int idx, int prev) {
-        if(idx >= color.size()) return 0;
-        
-        if(dp[idx][prev] != -1) return dp[idx][prev];
-        
-        int ans = 0;
-        if(color[idx] - 'a' != prev) {
-            ans = max(ans, neededTime[idx] + helper(color, neededTime, idx + 1, color[idx] - 'a'));
-        }
-        
-        ans = max(ans, helper(color, neededTime, idx + 1, prev));
-        return dp[idx][prev] = ans;
-    }
     int minCost(string colors, vector<int>& neededTime) {
-        memset(dp, -1, sizeof(dp));
-        int res = helper(colors, neededTime, 0, 26);
-        int tot = 0;
-        for(int ele : neededTime) tot += ele;
-        return tot - res;
+        int ans = 0, n = colors.size();
+        for(int i = 0; i < n;) {
+            int mx = neededTime[i];
+            char curr = colors[i];
+            int sum = 0;
+            while(i < n && colors[i] == curr) {
+                sum += neededTime[i];
+                mx = max(mx, neededTime[i]);
+                i++;
+            }
+            ans += (sum - mx);
+        }
+        return ans;
     }
 };
