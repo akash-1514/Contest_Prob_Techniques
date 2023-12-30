53e2b539e1284dc5eb9b4a566a4928e7200ff9e6
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-12-30 09:00:26 -0800

    Time: 0 ms (100.00%), Space: 6.4 MB (77.40%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -6,61 +6,39 @@ public:
         if(b != 0) pq.push({b, 'b'});
         if(c != 0) pq.push({c, 'c'});
         
-        string ans;
-        
-        while(pq.size() > 1)
-        {
-            pair<int, char>tp = pq.top();
-            pq.pop();
-            
-            if(tp.first >= 2)
-            {
-                ans.push_back(tp.second);
-                ans.push_back(tp.second);
-                tp.first -= 2;
-            }
-            else
-            {
-                ans.push_back(tp.second);
-                tp.first -= 1;
+        string ans = "";
+        while(pq.size() > 1) {
+            pair<int, char> tp1 = pq.top(); pq.pop();
+            if(tp1.first >= 2) {
+                ans += tp1.second;
+                ans += tp1.second;
+                tp1.first -= 2;
+            } else {
+                ans += tp1.second;
+                tp1.first--;
             }
             
-            pair<int, char>stop = pq.top();
-            pq.pop();
+            pair<int, char> tp2 = pq.top(); pq.pop();
             
-            if(stop.first >= 2 && stop.first >= tp.first)
-            {
-                ans.push_back(stop.second);
-                ans.push_back(stop.second);
-                stop.first -= 2;
-            }
-            else
-            {
-                ans.push_back(stop.second);
-                stop.first -= 1;
-            }
-
-            if(tp.first != 0)
-            {
-                pq.push(tp);
+            if(tp2.first >= 2 && tp2.first >= tp1.first) {
+                ans += tp2.second;
+                ans += tp2.second;
+                tp2.first -= 2;
+            } else {
+                ans += tp2.second;
+                tp2.first--;
             }
             
-            if(stop.first != 0)
-            {
-                pq.push(stop);
-            }
+            if(tp1.first > 0) pq.push(tp1);
+            if(tp2.first > 0) pq.push(tp2);
         }
         
-        if(!pq.empty())
-        {
-            if(pq.top().first >= 2)
-            {
-                ans.push_back(pq.top().second);
-                ans.push_back(pq.top().second);                
-            }
-            else
-            {
-                ans.push_back(pq.top().second);
+        if(!pq.empty()) {
+            if(pq.top().first >= 2) {
+                ans += pq.top().second;
+                ans += pq.top().second;
+            } else {
+                ans += pq.top().second;
             }
         }
         
