78784cddeff475eb9c58f23ab9f3e8d5481f8889
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2023-12-30 11:10:07 -0800

    Time: 1469 ms (5.11%), Space: 151.2 MB (41.05%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,30 +1,32 @@
-#define ppi pair<int, int>
 class Solution {
 public:
-    vector<int> getOrder(vector<vector<int>>& tasks) {
-        vector<vector<int>>vec;
-        for(int i = 0; i < tasks.size(); ++i) {
-            vec.push_back({tasks[i][0], tasks[i][1], i});
+    vector<int> getOrder(vector<vector<int>>& Tasks) {
+        int n = Tasks.size();
+        vector<vector<int>>tasks;
+        for(int i = 0; i < n; ++i) {
+            tasks.push_back({Tasks[i][0], Tasks[i][1], i});
         }
-        sort(vec.begin(), vec.end());
+        sort(tasks.begin(), tasks.end());
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        
         vector<int>ans;
-        priority_queue<ppi, vector<ppi>, greater<ppi>>pq;
-        int i = 0;
-        int time = vec[0][0];
-        while(i < vec.size()) {
-            while(i < vec.size() && vec[i][0] <= time) {
-                pq.push({vec[i][1], vec[i][2]});
-                i++;
-            }
-            
+        
+        long long time = 0, idx = 0;
+        
+        while(!pq.empty() || idx < n) {
             if(!pq.empty()) {
-                ans.push_back(pq.top().second);
-                time += pq.top().first;
-                pq.pop();
-            } else {
-                if(i < vec.size()) {
-                    time = vec[i][0];
+                auto tp = pq.top(); pq.pop();
+                int pTime = tp.first, t = tp.second;
+                time += pTime;
+                ans.push_back(t);
+                while(idx < n && tasks[idx][0] <= time) {
+                    pq.push({tasks[idx][1], tasks[idx][2]});
+                    idx++;
                 }
+            } else {
+                time = tasks[idx][0];
+                pq.push({tasks[idx][1], tasks[idx][2]});
+                idx++;
             }
         }
         
@@ -34,6 +36,5 @@ public:
         }
         
         return ans;
-        
     }
 };
