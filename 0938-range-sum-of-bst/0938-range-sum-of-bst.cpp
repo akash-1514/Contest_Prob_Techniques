324b91a6559add646ac1eebdaa8d50de7b5f7bf4
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-01-08 10:27:23 -0800

    Time: 97 ms (61.80%), Space: 64.9 MB (51.38%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -14,12 +14,16 @@ public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(root == NULL) return 0;
         
-        if(root->val >= low && root->val <= high) {
-            return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
-        } else if(root->val < low) {
-            return rangeSumBST(root->right, low, high);
-        } else {
-            return rangeSumBST(root->left, low, high);
+        int val = (root->val >= low && root->val <= high) ? root->val : 0;
+        
+        if(root->val > low) {
+            val += rangeSumBST(root->left, low, high);
         }
+        
+        if(root->val < high) {
+            val += rangeSumBST(root->right, low, high);
+        }
+        
+        return val;
     }
 };
