5af87802c6b4a0580e453b4405185313847f85a4
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-02-08 10:48:03 -0800

    Time: 50 ms (40.70%), Space: 17.8 MB (21.22%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     unordered_map<char, int>mp;
+    vector<int> freq;
+    int dp[26][26];
     int mod = 1e9 + 7;
-    unordered_map<string, int>dp;
-    int helper(int idx, int k, int maxBeauty) {
-        if(k == 0) {
-            if(maxBeauty == 0) return 1;
-            return 0;
-        }
-        if(idx >= 26) {
-            return 0;
+    int helper(int idx, int cnt, int k) {
+        if(idx >= freq.size()) {
+            return cnt == k;
         }
         
-        string state = to_string(idx) + "," + to_string(k) + to_string(maxBeauty);
-        if(dp.find(state) != dp.end()) return dp[state];
-         
+        if(cnt > k) return 0;
+        
+        if(dp[idx][cnt] != -1) return dp[idx][cnt];
+        
         int ans = 0;
-        
-        if(mp[idx + 'a'] > 0) {
-            ans = (ans + mp[idx + 'a'] * 1ll * helper(idx + 1, k - 1, maxBeauty - mp[idx + 'a'])) % mod;
+        if(freq[cnt] == freq[idx]) {
+            ans = (ans + freq[cnt] * 1ll * helper(idx + 1, cnt + 1, k)) % mod;
         }
         
-        ans = (ans + helper(idx + 1, k, maxBeauty)) % mod;
-        return dp[state] = ans;
+        ans = (ans + helper(idx + 1, cnt, k)) % mod;
+        return dp[idx][cnt] = ans;
     }
     int countKSubsequencesWithMaxBeauty(string s, int k) {
         for(char ch : s) mp[ch]++;
         if(mp.size() < k) return 0;
-        priority_queue<int>pq;
-        for(auto &it : mp) pq.push(it.second);
-        int maxBeauty = 0;
-        int cnt = 0;
-        while(!pq.empty() && cnt < k) {
-            maxBeauty += pq.top();
-            pq.pop();
-            cnt++;
-        }
-        
-        return helper(0, k, maxBeauty);
+        for(auto &it : mp) freq.push_back(it.second);
+        sort(freq.rbegin(), freq.rend());
+        memset(dp, -1, sizeof(dp));
+        return helper(0, 0, k);
     }
 };
