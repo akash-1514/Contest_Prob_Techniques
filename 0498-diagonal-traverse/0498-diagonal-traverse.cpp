8e60310bcae846fd3096438a66c97ef7f1549e3e
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-03-07 12:00:21 -0800

    Time: 58 ms (14.41%), Space: 32.5 MB (7.67%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -2,37 +2,21 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        vector<int>ans;
-        bool fl = true;
+        map<int, vector<int>>mp;
         for(int i = 0; i < n; ++i) {
-            int r = i, c = 0;
-            vector<int> d;
-            while(r >= 0 && c < m) {
-                d.push_back(matrix[r][c]);
-                r--, c++;
+            for(int j = 0; j < m; ++j) {
+                mp[i + j].push_back(matrix[i][j]);
             }
-            if(!fl) {
-                reverse(d.begin(), d.end());
-            }
-            
-            for(int ele : d) {
-                ans.push_back(ele);
-            }
-            
-            fl = !fl;
         }
         
-        for(int j = 1; j < m; ++j) {
-            int r = n - 1, c = j;
-            vector<int>d;
-            while(r >= 0 && c < m) {
-                d.push_back(matrix[r][c]);
-                r--, c++;
+        vector<int>ans;
+        bool fl = true;
+        for(auto &it : mp) {
+            vector<int> vec = it.second;
+            if(fl) {
+                reverse(vec.begin(), vec.end());
             }
-            if(!fl) {
-                reverse(d.begin(), d.end());
-            }
-            for(int ele : d) {
+            for(int ele : vec) {
                 ans.push_back(ele);
             }
             fl = !fl;
