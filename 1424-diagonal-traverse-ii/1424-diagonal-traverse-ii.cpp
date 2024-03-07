38aea24390e80f538f24264678168e4254e19858
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-03-07 12:22:52 -0800

    Time: 244 ms (19.95%), Space: 102.2 MB (29.58%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        int n = nums.size();
+        
+        map<int, vector<int>>mp;
+        for(int i = n - 1; i >= 0; --i) {
+            int m = nums[i].size();
+            for(int j = m - 1; j >= 0; --j) {
+                mp[i + j].push_back(nums[i][j]);
+            }
+        }
+        
+        vector<int>ans;
+        int diagonal = 0;
+        while(mp.find(diagonal) != mp.end()) {
+            for(int ele : mp[diagonal]) {
+                ans.push_back(ele);
+            }
+            diagonal++;
+        }
+        
+        
+        return ans;
+    }
+};
