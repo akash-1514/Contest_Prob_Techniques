f2cefcb92b7f3a61c2c9f439fc3b744db14658f9
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-03-11 08:02:31 -0700

    Time: 170 ms (12.25%), Space: 93.9 MB (50.00%) - LeetHub

--- 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    long long dp[100001];
-    long long helper(vector<int>&nums, int idx, int k) {
-        // base case
-        
-        if(idx >= nums.size() - 2) {
+    long long dp[100001][3];
+    long long helper(vector<int>&arr, int idx, int k, int cnt) {
+        if(idx >= arr.size()) {
             return 0;
         }
         
-        if(dp[idx] != -1) return dp[idx];
+        if(dp[idx][cnt] != -1) return dp[idx][cnt];
         
-        long long ans = LONG_MAX;
-        for(int i = idx; i < min((int)nums.size(), idx + 3); ++i) {
-            long long val = max(0, k - nums[i]) + helper(nums, i + 1, k);
-            ans = min(ans, val);
+        long long mn = 1e18;
+        
+        if(cnt > 0) {
+            mn = min(mn, max(0, k - arr[idx]) + helper(arr, idx + 1, k, 2));
+            mn = min(mn, helper(arr, idx + 1, k, cnt - 1));
+        } else {
+            mn = min(mn, max(0, k - arr[idx]) + helper(arr, idx + 1, k, 2));
         }
         
-        return dp[idx] = ans;
+        return dp[idx][cnt] = mn;
     }
     long long minIncrementOperations(vector<int>& nums, int k) {
         memset(dp, -1, sizeof(dp));
-        return helper(nums, 0, k);
+        long long mn = 1e18;
+        for(int i = 0; i < 3; ++i) {
+            if(nums[i] >= k) {
+                mn = min(mn, helper(nums, i + 1, k, 2));
+            } else {
+                mn = min(mn, (k - nums[i]) + helper(nums, i + 1, k, 2));
+            }
+        }
+        return mn;
     }
 };
