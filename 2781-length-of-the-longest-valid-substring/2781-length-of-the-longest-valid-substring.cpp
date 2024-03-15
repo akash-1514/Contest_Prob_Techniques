d158972a2901611eb86d11130e6e5f365df26522
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-03-15 11:22:14 -0700

    Time: 1797 ms (5.16%), Space: 256.5 MB (23.71%) - LeetHub

---
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestValidSubstring(string word, vector<string>& forbidden) {
+        unordered_map<string, int>mp;
+        unordered_map<int, int>mark;
+        for(auto &str : forbidden) mp[str]++;
+        int n = word.size();
+        for(int i = 0; i < n; ++i) {
+            string sub = "";
+            for(int j = i; j < min(n, i + 10); ++j) {
+                sub += word[j];
+                if(mp.find(sub) != mp.end()) {
+                    mark[j] = max(mark[j], i);
+                }
+            }
+        }
+        
+        int i = 0, ans = 0;
+        for(int j = 0; j < n; ++j) {
+            
+            if(mark.find(j) != mark.end()) {
+                while(i <= mark[j]) {
+                    i++;
+                }
+            }
+            
+            ans = max(ans, j - i + 1);
+        }
+        return ans;
+    }
+};
