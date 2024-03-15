46fdcb9d159041501d85b221457702ba86371ee5
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-03-15 07:06:33 -0700

    Time: 119 ms (52.36%), Space: 100.9 MB (52.67%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int n = nums.size(), totSum = 0;
-        
-        for(int ele : nums) {
-            totSum += ele;
-        }
-        
-        if(x > totSum) return -1;
-        
-        unordered_map<int, int>mp;
-        int target = totSum - x, sum = 0, mx = INT_MIN;
-        mp[0] = -1;
-        // extra condition we missed
-        if(target == 0) return n;
-        
-        for(int i = 0; i < n; ++i) {
-            sum += nums[i];
-            if(mp.find(sum - target) != mp.end()) {
-                mx = max(mx, i - mp[sum - target]);
+        cout << nums.size() << "\n";
+        int totSum = 0;
+        for(int ele : nums) totSum += ele;
+        int remSum = totSum - x;
+        if(remSum < 0) return -1;
+        long long i = 0, sum = 0, mx = -1e9;
+        for(int j = 0; j < nums.size(); ++j) {
+            sum += nums[j];
+            while(sum > remSum) {
+                sum -= nums[i++];
             }
-            mp[sum] = i;
+            if(sum == remSum) {
+                mx = max(mx, j - i + 1);
+            }
         }
-        
-        if(mx == INT_MIN) return -1;
-        return n - mx;
+        return (mx == -1e9) ? -1 : (nums.size() - mx);
     }
 };
