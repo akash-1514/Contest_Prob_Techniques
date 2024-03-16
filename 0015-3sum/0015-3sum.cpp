6e4da4c6cb749aba3067132ef5730a00af62e61b
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-03-16 01:35:16 -0700

    Time: 50 ms (95.45%), Space: 27 MB (79.44%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,14 +4,13 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>>ans;
-        set<vector<int>>st;
         for(int i = 0; i < n; ++i) {
             int start = i + 1, end = n - 1;
             int req = -1 * nums[i];
             while(start < end) {
                 int currSum = nums[start] + nums[end];
                 if(currSum == req) {
-                    st.insert({nums[i], nums[start], nums[end]});
+                    ans.push_back({nums[i], nums[start], nums[end]});
                     start++, end--;
                     while(start < end && nums[start] == nums[start - 1]) start++;
                     while(start < end && nums[end] == nums[end + 1]) end--;
@@ -21,11 +20,9 @@ public:
                     start++;
                 }
             }
+            while(i < n - 1 && nums[i] == nums[i + 1]) i++;
         }
         
-        for(auto &it : st) {
-            ans.push_back(it);
-        }
         return ans;
     }
 };
