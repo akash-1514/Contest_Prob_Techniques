6730c6652130e060b32397d85a45926a855db159
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-03-23 13:27:34 +0530

    Time: 67 ms (76.70%), Space: 54.7 MB (70.94%) - LeetHub

---
+++ 2967-minimum-cost-to-make-array-equalindromic/2967-minimum-cost-to-make-array-equalindromic.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool check(int x) {
+        int temp = x;
+        int rev = 0;
+        while(x != 0) {
+            int r = x % 10;
+            rev = rev * 10 + r;
+            x /= 10;
+        }
+        return rev == temp;
+    }
+    long long minimumCost(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ele = nums[nums.size() / 2];
+        
+        int left = ele, right = ele;
+        while(left > 0) {
+            if(check(left)) {
+                break;
+            }
+            left--;
+        }
+        
+        while(1) {
+            if(check(right)) {
+                break;
+            }
+            right++;
+        }
+        
+        cout << left << ": " << right << "\n";
+        
+        long long cost1 = 0, cost2 = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            cost1 += abs(nums[i] - left);
+        }
+        
+        for(int i = 0; i < nums.size(); ++i) {
+            cost2 += abs(nums[i] - right);
+        }
+        
+        return min(cost1, cost2);
+    }
+};
