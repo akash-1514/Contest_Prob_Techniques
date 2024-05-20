57cb4d4e5e1c2499ddd77de16b77e0b5a17ec11a
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-05-20 10:25:40 +0530

    Time: 4 ms (55.14%), Space: 8.2 MB (88.36%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    void helper(vector<int>&nums, int idx, int &ans, int res) {
-        if(idx >= nums.size()) {
-            ans += res;
-            return;
-        }
-        
-        helper(nums, idx + 1, ans, res ^ nums[idx]);
-        helper(nums, idx + 1, ans, res);
-    }
     int subsetXORSum(vector<int>& nums) {
-        int ans = 0;
-        helper(nums, 0, ans, 0);
+        int n = nums.size(), ans = 0;
+        for(int i = 0; i < (1 << n); ++i) {
+            int res = 0;
+            for(int j = 0; j < 32; ++j) {
+                if((i & (1 << j)) != 0) {
+                    res ^= nums[j];
+                }
+            }
+            ans += res;
+        }
         return ans;
     }
 };
