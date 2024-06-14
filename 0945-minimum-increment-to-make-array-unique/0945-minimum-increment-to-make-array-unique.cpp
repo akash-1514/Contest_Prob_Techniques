01744ba8760316a3f3575e6d28a0cb524c7651e7
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-06-15 01:18:26 +0530

    Time: 81 ms (97.52%), Space: 73.9 MB (13.36%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int mx = *max_element(nums.begin(), nums.end());
+        vector<int> freq(n + mx + 1, 0);
+        for(int ele : nums) freq[ele]++;
         int ans = 0;
-        for(int i = 1; i < nums.size(); ++i) {
-            if(nums[i] <= nums[i - 1]) {
-                ans += (nums[i - 1] - nums[i] + 1);
-                nums[i] += (nums[i - 1] - nums[i] + 1);
-            }
+        for(int i = 0; i < n + mx + 1; ++i) {
+            if(freq[i] <= 1) continue;
+            freq[i + 1] += (freq[i] - 1);
+            ans += (freq[i] - 1);
+            freq[i] = 1;
         }
         return ans;
     }
