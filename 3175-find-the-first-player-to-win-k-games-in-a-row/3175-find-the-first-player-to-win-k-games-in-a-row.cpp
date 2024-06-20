1c46ae2c8f1d54d36fd3959e8d17325d7df54de4
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-06-21 01:04:29 +0530

    Time: 145 ms (79.92%), Space: 118.9 MB (96.49%) - LeetHub

--- 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -2,36 +2,16 @@ class Solution {
 public:
     int findWinningPlayer(vector<int>& skills, int k) {
         int n = skills.size();
-        deque<int>q;
-        int mx = 0, idx = 0;
-        for(int i = 0; i < n; ++i) {
-            q.push_back(i);
-            if(skills[i] > mx) {
-                mx = skills[i];
-                idx = i;
+        int prev = skills[0], ans = 0, cnt = 0;
+        for(int i = 1; i < n; ++i) {
+            if(prev < skills[i]) {
+                prev = skills[i];
+                ans = i;
+                cnt = 0;
             }
+            cnt++;
+            if(cnt >= k) return ans;
         }
-        
-        int prev = 0, cnt = 0;
-        for(int i = 0; i < n; ++i) {
-            int idx1 = q.front(); q.pop_front();
-            int idx2 = q.front(); q.pop_front();
-            if(skills[idx1] > skills[idx2]) {
-                q.push_front(idx1);
-                q.push_back(idx2);
-            } else {
-                q.push_front(idx2);
-                q.push_back(idx1);
-            }
-            
-            if(q.front() == prev) {
-                cnt++;
-            } else {
-                prev = q.front();
-                cnt = 1;
-            }
-            if(cnt == k) return q.front();
-        }
-        return idx;
+        return ans;
     }
 };
