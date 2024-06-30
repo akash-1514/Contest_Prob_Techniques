1b0241c4e5dab1609945f65fb1fb130050ec3172
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-06-30 21:00:25 +0530

    Time: 349 ms (77.48%), Space: 143.4 MB (64.54%) - LeetHub

---
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -0,0 +1,110 @@
+class DSU {
+public:
+	vector<int> rank, size, parent;
+	DSU(int n) {
+		rank.resize(n + 1, 0);
+		size.resize(n + 1);
+		parent.resize(n + 1, 0);
+		for (int i = 0; i <= n; ++i) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+
+	int findPar(int u) {
+		if (parent[u] == u) return u;
+		return parent[u] = findPar(parent[u]);
+	}
+
+	void unionByRank(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+		if (par_u == par_v) return;
+		int rank_u = rank[par_u];
+		int rank_v = rank[par_v];
+		if (rank_v == rank_u) {
+			rank[par_u]++;
+			parent[par_v] = par_u;
+		} else if (rank_u > rank_v) {
+			parent[par_v] = par_u;
+		} else {
+			parent[par_u] = par_v;
+		}
+	}
+
+	void unionBySize(int u, int v) {
+		int par_u = findPar(u);
+		int par_v = findPar(v);
+
+		if(par_u == par_v) return;
+
+		int size_u = size[par_u];
+		int size_v = size[par_v];
+
+		if(size_v > size_u) {
+			parent[par_u] = par_v;
+			size[par_v] += size[par_u];
+		} else {
+			parent[par_v] = par_u;
+			size[par_u] += size[par_v];
+		}
+   	}
+};
+class Solution {
+public:
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        DSU ds1(n), ds2(n);
+        int ans = 0;
+        for(auto &e : edges) {
+            if(e[0] == 3) {
+                int par_u = ds1.findPar(e[1]);
+                int par_v = ds1.findPar(e[2]);
+                
+                if(par_u != par_v) {
+                    ds1.unionByRank(par_u, par_v);
+                    ds2.unionByRank(par_u, par_v);
+                } else {
+                    ans++;
+                }
+            }
+        }
+        
+        for(auto &e : edges) {
+            int type = e[0], u = e[1], v = e[2];
+            if(type == 1) {
+                int par_u = ds1.findPar(u);
+                int par_v = ds1.findPar(v);
+                if(par_u != par_v) {
+                    ds1.unionByRank(par_u, par_v); 
+                } else {
+                    ans++;
+                }
+            } else if(type == 2) {
+                int par_u = ds2.findPar(u);
+                int par_v = ds2.findPar(v);
+                if(par_u != par_v) {
+                    ds2.unionByRank(par_u, par_v); 
+                } else {
+                    ans++;
+                }
+            }
+        }
+        
+        int countComponents1 = 0, countComponents2 = 0;
+        for(int i = 1; i <= n; ++i) {
+            if(ds1.parent[i] == i) {
+                countComponents1++;
+            }
+            
+            if(ds2.parent[i] == i) {
+                countComponents2++;
+            }
+        }
+        
+        if(countComponents1 > 1 || countComponents2 > 1) {
+            return -1;
+        }
+        
+        return ans;
+    }
+};
