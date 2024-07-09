45488994ce87f1310e0b0cf18a5e786a517faeb0
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-07-09 20:46:28 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (99.12%) - LeetHub

--- 1718-construct-the-lexicographically-largest-valid-sequence/1718-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1718-construct-the-lexicographically-largest-valid-sequence/1718-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    vector<int>vis;
-    bool helper(int n, int cnt, int idx, vector<int>&res) {
-        if(cnt == n) {
+    bool helper(vector<int>&ans, int n, int idx, int vis) {
+        if(idx >= ans.size()) {
             return true;
         }
         
-        if(res[idx] == 0) {
+        if(ans[idx] == 0) {
+            // place the ith element 
             for(int i = n; i >= 1; --i) {
-                if(vis[i] == 0) {
-                    if(i == 1) {
-                        res[idx] = 1;
-                        vis[i] = 1;
-                        if(helper(n, cnt + 1, idx + 1, res)) return true;
-                        vis[i] = 0;
-                        res[idx] = 0;
-                    } else if(i > 1 && idx + i < 2 * n - 1 && res[idx + i] == 0) {
-                        res[idx] = res[idx + i] = i;
-                        vis[i] = 1;
-                        if(helper(n, cnt + 1, idx + 1, res)) return true;
-                        vis[i] = 0;
-                        res[idx] = res[idx + i] = 0;
+                if((vis & (1 << i)) == 0) {
+                    if(i != 1) {
+                        if(idx + i < ans.size()) {
+                            if(ans[idx + i] == 0) {
+                                ans[idx] = i;
+                                ans[idx + i] = i;
+                                if(helper(ans, n, idx + 1, (vis | (1 << i)))) return true;
+                                ans[idx] = 0;
+                                ans[idx + i] = 0;
+                            }
+                        }
+                    } else {
+                        ans[idx] = 1;
+                        if(helper(ans, n, idx + 1, (vis | (1 << i)))) return true;
+                        ans[idx] = 0;
                     }
                 }
             }
         } else {
-            if(helper(n, cnt, idx + 1, res)) return true;
+            if(helper(ans, n, idx + 1, vis)) return true;
         }
-        
         return false;
     }
     vector<int> constructDistancedSequence(int n) {
-        vis.resize(n + 1, 0);
-        vector<int> res(2 * n - 1, 0);
-        helper(n, 0, 0, res);
-        return res;
+        int size = 2 * (n - 1) + 1;
+        vector<int> ans(size, 0);
+        helper(ans, n, 0, 0);
+        return ans;
     }
 };
