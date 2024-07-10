a208f222a936f020e5feaa6aeeecfed3c0ffbf61
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-07-10 09:40:44 +0530

    Time: 24 ms (35.84%), Space: 12 MB (80.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int helper(string &s, string &t, int idx1, int idx2) {
-        if(idx2 == t.size()) return 1;
-        if(idx1 == s.size()) return 0;
+    int dp[1001][1001];
+    int helper(string &s, string& t, int idx1, int idx2) {
+        if(idx2 >= t.size()) {
+            return 1;
+        }
+        
+        if(idx1 >= s.size()) {
+            return 0;
+        }
         
         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
         
+        int ways = 0;
         if(s[idx1] == t[idx2]) {
-            return dp[idx1][idx2] = helper(s, t, idx1 + 1, idx2 + 1) + helper(s, t, idx1 + 1, idx2);
-        } else {
-            return dp[idx1][idx2] = helper(s, t, idx1 + 1, idx2);
+            ways += helper(s, t, idx1 + 1, idx2 + 1);
         }
+        
+        ways += helper(s, t, idx1 + 1, idx2);
+        return dp[idx1][idx2] = ways;
     }
     int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        dp.resize(n + 1, vector<int>(m + 1, -1));
+        memset(dp, -1, sizeof(dp));
         return helper(s, t, 0, 0);
     }
 };
