877fe648c21072e12e2b6041d672a147b15729de
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-07-10 11:31:34 +0530

    Time: 26 ms (68.37%), Space: 10.3 MB (96.05%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int dp[101][101];
-    int helper(vector<int>&cuts, int i, int j) {
-        if(i > j) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int minCost = 1e9;
-        for(int k = i; k <= j; ++k) {
-            int cost = (cuts[j + 1] - cuts[i - 1]) + helper(cuts, i, k - 1) + helper(cuts, k + 1, j);
-            minCost = min(minCost, cost);
+    int helper(vector<int>&cost, int l, int r) {
+        if(l > r) {
+            return 0;
         }
-        return dp[i][j] = minCost;
+        
+        if(dp[l][r] != -1) return dp[l][r];
+        
+        int mn = 1e9;
+        for(int i = l; i <= r; ++i) {
+            int res = cost[r + 1] - cost[l - 1];
+            mn = min(mn, res + helper(cost, l, i - 1) + helper(cost, i + 1, r));
+        }
+        return dp[l][r] = mn;
     }
     int minCost(int n, vector<int>& cuts) {
         sort(cuts.begin(), cuts.end());
         cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
+        
         memset(dp, -1, sizeof(dp));
+        
+        
         return helper(cuts, 1, cuts.size() - 2);
     }
 };
