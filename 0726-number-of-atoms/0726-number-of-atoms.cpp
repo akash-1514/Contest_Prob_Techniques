a5057ed4010d6023c264d967756151b0442bf6ab
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-07-14 12:37:10 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (76.47%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.cpp
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -2,63 +2,56 @@ class Solution {
 public:
     string countOfAtoms(string s) {
         int n = s.size();
+        stack<int>st;
+        vector<int> prefix(n, 0);
+        int prod = 1;
+        string num = "";
+        for(int i = n - 1; i >= 0; --i) {
+            if(s[i] >= '0' && s[i] <= '9') {
+                num += s[i];
+            } else if(s[i] == ')') {
+                if(num != "") {
+                    reverse(num.begin(), num.end());
+                    st.push(stoi(num));
+                    prod *= stoi(num);
+                    num = "";
+                } else {
+                    st.push(1);
+                }
+            } else if(s[i] == '(') {
+                prod /= st.top();
+                st.pop();
+            } else {
+                num = "";
+            }
+            prefix[i] = prod;
+        }
+        
         map<string, int>mp;
-        stack<pair<string, int>>st;
+        
         for(int i = 0; i < n; ++i) {
+            int start = i;
             if(s[i] >= 'A' && s[i] <= 'Z') {
-                string chem = "";
-                chem += s[i];
-                while(i < n && (s[i + 1] >= 'a' && s[i + 1] <= 'z')) {
+                string chem = ""; chem += s[i];
+                while(i < n - 1 && (s[i + 1] >= 'a' && s[i + 1] <= 'z')) {
                     chem += s[i + 1];
                     i++;
                 }
-                      
-                int num = 0;
-                if(s[i + 1] >= '0' && s[i + 1] <= '9') {
-                    while(i < n - 1 && (s[i + 1] >= '0' && s[i + 1] <= '9')) {
-                        num = num * 10 + (s[i + 1] - '0');
-                        i++;
-                    }
-                }
                 
-                if(num == 0) num = 1;
-            
-                st.push({chem, num});
-                
-            } else if(s[i] == '(') {
-                st.push({"(", 0});
-            } else if(s[i] >= ')') {
                 int num = 0;
                 while(i < n - 1 && (s[i + 1] >= '0' && s[i + 1] <= '9')) {
                     num = num * 10 + (s[i + 1] - '0');
                     i++;
                 }
                 
-                if(num == 0) num = 1;
-                
-                stack<pair<string, int>>st2;
-                while(!st.empty() && st.top().first != "(") {
-                    int x = st.top().second;
-                    string chem = st.top().first;
-                    st2.push({chem, x * num});
-                    st.pop();
-                }        
-                
-                
-                st.pop();
-                
-                while(!st2.empty()) {
-                    st.push(st2.top());
-                    st2.pop();
+                if(num != 0) {
+                    mp[chem] += prefix[start] * num;
+                } else {
+                    mp[chem] += prefix[start];
                 }
             }
         }
         
-        while(!st.empty()) {
-            mp[st.top().first] += st.top().second;
-            st.pop();
-        }
-                    
         string ans = "";
         for(auto &it : mp) {
             ans += it.first;
@@ -66,6 +59,7 @@ public:
                 ans += to_string(it.second);
             }
         }
+        
         return ans;
     }
 };
