b732e58ffe4a60907e52f20c341719081d56282a
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-07-23 00:59:59 +0530

    Time: 1052 ms (43.85%), Space: 17.4 MB (48.26%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
+    int dp[71][5000];
+    int helper(vector<vector<int>>&mat, int row, int target, int sum) {
+        int n = mat.size(), m = mat[0].size();
+        if(row == n) {
+            return abs(sum - target);
+        }
+        
+        if(dp[row][sum] != -1) return dp[row][sum];
+        
+        int mn = 1e9;
+        for(int col = 0; col < m; ++col) {
+            mn = min(mn, helper(mat, row + 1, target, sum + mat[row][col]));
+        }
+        
+        return dp[row][sum] = mn;
+    }
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
         int n = mat.size(), m = mat[0].size();
         
-        int mx = 0;
-        for(int i = 0; i < n; ++i) {
-            int mxx = 0;
-            for(int j = 0; j < m; ++j) {
-                mxx = max(mxx, mat[i][j]);
-            }
-            mx += mxx;
-        }
-        
-        mx = max(mx, target);
-        
-        vector<vector<int>> dp(n + 1, vector<int>(mx + 1, 0));
-        
-        dp[n][0] = 1;
-        
-        for(int row = n - 1; row >= 0; --row) {
-            for(int k = 0; k <= mx; ++k) {
-                bool res = false;
-                for(int col = 0; col < m; ++col) {
-                    if(mat[row][col] <= k) {
-                        res |= dp[row + 1][k - mat[row][col]];
-                    }
-                }
-                dp[row][k] = res;
-            }
-        }
-        
-        int ans = 1e9;
-        
-        for(int i = 0; i <= mx; ++i) {
-            if(dp[0][i]) {
-                ans = min(ans, abs(i - target));
-            }
-        }
-        
-        return ans;
+        memset(dp, -1, sizeof(dp));
+        return helper(mat, 0, target, 0);
     }
 };
