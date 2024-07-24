748372d01d339a900fe7b32a64045ab9e6b02ff1
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-07-24 21:30:13 +0530

    Time: 234 ms (68.83%), Space: 88.9 MB (55.84%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    unordered_map<int, int>mp;
+public:
+    int convert(string &s) {
+        int res = 0;
+        for(char ch : s) {
+            res = res * 10 + (mp[ch - '0']);
+        }
+        return res;
+    }
+    
+    static bool cmp(pair<int, int>&a, pair<int, int>&b) {
+        if(a.first == b.first) {
+            return a.second < b.second;
+        }
+        return a.first < b.first;
+    }
+
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i <= 9; ++i) {
+            mp[i] = mapping[i];
+        }
+        
+        vector<pair<int, int>>vec;
+        for(int i = 0; i < n; ++i) {
+            string str = to_string(nums[i]);
+            int changed = convert(str);
+            vec.push_back({changed, i});
+        }
+        
+        sort(vec.begin(), vec.end(), cmp);
+        
+        vector<int>ans;
+        for(int i = 0; i < n; ++i) {
+            ans.push_back(nums[vec[i].second]);
+        }
+        return ans;
+    }
+};
