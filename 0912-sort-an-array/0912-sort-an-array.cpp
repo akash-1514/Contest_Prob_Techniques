97a08ce3cb119cb56aa93eef0bb7ede1901b3292
akash-1514 <135205515+akash-1514@users.noreply.github.com>
2024-07-25 15:35:20 +0530

    Time: 2841 ms (5.00%), Space: 72.4 MB (57.17%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findPartition(vector<int>&nums, int start, int end) {
+        int pivot = nums[start];
+        int i = start, j = end;
+        while(i < j) {
+            while(i < end && nums[i] <= pivot) i++;
+            while(j > start && nums[j] > pivot) j--;
+            
+            if(i < j) {
+                swap(nums[i], nums[j]);
+            }
+        }
+        
+        swap(nums[start], nums[j]);
+        return j;
+    }
+    
+    void quickSort(vector<int>&nums, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+        
+        int partition = findPartition(nums, start, end);
+        
+        quickSort(nums, start, partition - 1);
+        quickSort(nums, partition + 1, end);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
